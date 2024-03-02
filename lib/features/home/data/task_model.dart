aebe165fcee21c49772704b962eb492361099417
Hussein Yasser <119596187+Hussein2077@users.noreply.github.com>
2024-03-02 15:36:59 +0200

    .

--- lib/features/home/data/task_model.dart
+++ lib/features/home/data/task_model.dart
@@ -1,8 +1,10 @@
-class TaskModel {
- final String taskName;
- final String? id;
- final String dueDate;
- final int isDone;
+import 'package:equatable/equatable.dart';
+
+class TaskModel extends Equatable {
+  final String taskName;
+  final String? id;
+  final String dueDate;
+  final int isDone;
 
   TaskModel({
     required this.taskName,
@@ -10,21 +12,26 @@ class TaskModel {
     this.id,
     required this.isDone,
   });
- // Convert the TaskModel to a map for database operations
- Map<String, dynamic> toMap() {
-   return {
-     'id': id,
-     'taskName': taskName,
-     'dueDate': dueDate,
-     'isDone': isDone,
-   };
- }
- factory TaskModel.fromMap(Map<String, dynamic> map) {
-   return TaskModel(
-     id: map['id'],
-     taskName: map['taskName'],
-     dueDate: map['dueDate'],
-     isDone: map['isDone'],
-   );
- }
+
+  // Convert the TaskModel to a map for database operations
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'taskName': taskName,
+      'dueDate': dueDate,
+      'isDone': isDone,
+    };
+  }
+
+  @override
+  List<Object?> get props => [id, taskName, dueDate, isDone];
+
+  factory TaskModel.fromMap(Map<String, dynamic> map) {
+    return TaskModel(
+      id: map['id'],
+      taskName: map['taskName'],
+      dueDate: map['dueDate'],
+      isDone: map['isDone'],
+    );
+  }
 }
